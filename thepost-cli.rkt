405da8354b4fc6cc6d895847025fbf39ac364f86
Lehi Toskin <lehi_toskin@lavabit.com>
2013-05-13 13:55:13 -0700

    Added license and working CLI NNTP program

---
+++ thepost-cli.rkt
@@ -0,0 +1,73 @@
+#!/usr/bin/env racket
+#lang racket
+; doh.rkt
+; simple cli nntp client
+; tips taken from: http://download.racket-lang.org/docs/5.0/html/net/nntp.html
+;
+; Copyright (C) 2013 Lehi Toskin
+;
+; This program is free software: you can redistribute it and/or modify
+; it under the terms of the GNU General Public License as published by
+; the Free Software Foundation, either version 3 of the License, or
+; (at your option) any later version.
+;
+; This program is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+; GNU General Public License for more details.
+;
+; You should have received a copy of the GNU General Public License
+; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;
+; TODO:
+; allow the user to exit the program whenever in the loop
+; allow the user to change newsgroup(/address?) in the loop
+; allow the user to define sender/receiver/connecting port?
+; add a sweet GUI
+(require net/nntp)
+
+; print out the header/body contents in a sane manner
+(define printer
+  (lambda (lst)
+    (if (null? lst)
+      (display "Empty list!\n")
+      (cond ((null? (rest lst)) (first lst))
+	    (else (printf "~a\n" (first lst)) (printer (rest lst)))))))
+
+(define nntp-client
+  (lambda ()
+    (display "Rudimentary NNTP Client!\n")
+    ;(display "Sending port: ")
+    (define sender 119) ;(read))
+    ;(display "Input port: ")
+    (define receiver 119) ;(read))
+    (display "Server to connect to: ")
+    (define server (symbol->string (read)))
+    (display "Newsgroup to read: ")
+    (define newsgroup (symbol->string (read)))
+    (printf "\n\nsender: ~a\nreceiver: ~s\nserver: ~a\nnewsgroup: ~a\n"
+	    sender receiver server newsgroup)
+    (let ((communicator (connect-to-server server #|port-number|#)))
+      (define-values (total start finish) (open-news-group communicator newsgroup))
+      (let loop ()
+	(printf "\nWhat message would you like to read?\nIndex of ~a from ~a to ~a: "
+		total start finish)
+	(let [(index (read))]
+	  (display "Choices: header | body | quit: ")
+	  (let [(choice (read))]
+	    (if (string=? (symbol->string choice) "quit")
+	      [(display "Good bye!\n")
+	       (disconnect-from-server communicator)
+	       (exit)]
+	      (cond [(string=? (symbol->string choice) "header")
+		     (let ((headers (head-of-message communicator index)))
+			(newline)
+			(printer headers))]
+		    [(string=? (symbol->string choice) "body")
+		     (let ((bodies (body-of-message communicator index)))
+			(newline)
+			(printer bodies))]
+		    (else (printf "I don't understand ~a" (symbol->string choice)))))))
+	(loop)))))
+
+(nntp-client)
